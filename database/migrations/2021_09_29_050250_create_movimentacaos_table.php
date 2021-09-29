7c7046a5c277bae1aaf6115ea6f5ed315d9d2bbe
Pedro Paulo Magno <ppfmagno@gmail.com>
2021-09-29 03:37:07 -0300

    Correção da migração do modelo de movimentos

--- database/migrations/2021_09_29_050250_create_movimentacaos_table.php
+++ database/migrations/2021_09_29_050250_create_movimentacaos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMovimentacoesTable extends Migration
+class CreateMovimentacaosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMovimentacoesTable extends Migration
      */
     public function up()
     {
-        Schema::create('movimentacoes', function (Blueprint $table) {
+        Schema::create('movimentacaos', function (Blueprint $table) {
             $table->id();
 
             $table->string('sku');
@@ -30,6 +30,6 @@ class CreateMovimentacoesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movimentacoes');
+        Schema::dropIfExists('movimentacaos');
     }
 }
